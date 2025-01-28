221b50f33b9f5f786ffd27902799f05f9db875eb
Telmen Enkhtuvshin <132520860+Telmen17@users.noreply.github.com>
2025-01-27 16:39:01 -0800

    Fixed Lab 1 Shell Script File for final submission
    Added confirmation echo messages for error handling.

--- create_files_with_subdirs.sh
+++ create_files_with_subdirs.sh
@@ -11,13 +11,16 @@ mkdir -p "$current_datetime"
 languages=("Python" "Java" "C++" "JavaScript" "Ruby" "C" "Swift" "Kotlin" "PHP" "Rust")
 
 # Print a confirmation message
-echo "Directory '$current_datetime' created successfully."
+echo "Directory '$current_datetime' created successfully..."
 
 # Create subdirectories
 for i in {101..110}; do
     # Subdirectory name
     subdir="$current_datetime/file$i"
     mkdir -p "$subdir"
+
+    # Subdirectory creation confirmation
+    echo "Subdirectory $subdir created successfully..."
 
     # Create files inside the subdirectory
     for j in {501..510}; do
@@ -27,9 +30,11 @@ for i in {101..110}; do
         langIndex=$(( (j - 501) % ${#languages[@]} ))
         # Creating file with
         echo "${languages[$langIndex]}" > "$file_name"
+
+        # File creation confirmation
+        echo "File $file_name created and written successfully..."
     done
 done
 
 # Print a confirmation message
-echo "Subdirectories file101 to file110 with tuser501.txt to tuser510.txt files created inside '$current_datetime'."
-
+echo "Subdirectories file101 to file110 with tuser501.txt to tuser510.txt files successfully created inside '$current_datetime'..."
